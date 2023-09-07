9913789f3a15d8b2b1b33d1e06ab488251b7edbe
George Halterman <113994972+Agoews@users.noreply.github.com>
2023-09-07 09:14:41 -0700

    completed the react practice but needed to udate the json test files since my query for count is done once in a useEffect on page load

--- src/pokemon-list.jsx
+++ src/pokemon-list.jsx
@@ -1,7 +1,45 @@
-// You can retrieve the pokemons by calling the following API
-// Make sure to replace limit and offset with the appropriate values
-// https://pokeapi.co/api/v2/pokemon?limit=5&offset=0
+import React, { useState, useEffect } from 'react';
 
-const PokemonList = () => {};
+const PokemonList = () => {
+  const [pokemon, setPokemon] = useState([]);
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon?limit=5&offset=0`)
+      .then((response) => response.json())
+      .then((data) => {
+        setTotal(data.count);
+        setPokemon(data.results);
+      });
+  }, []);
+
+  const loadMore = () => {
+    fetch(`https://pokeapi.co/api/v2/pokemon?limit=5&offset=${pokemon.length}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setPokemon([...pokemon, ...data.results]);
+      });
+  };
+
+  return (
+    <>
+      <ul role="list">
+        {pokemon.map((poke, index) => (
+          <li role="listitem" key={index}>
+            {poke.name}
+          </li>
+        ))}
+      </ul>
+      {pokemon.length < total ? (
+        <button type="button" role="button" onClick={loadMore}>
+          Load more
+        </button>
+      ) : null}
+      <div>
+        Displaying {pokemon.length} of {total} results
+      </div>
+    </>
+  );
+};
 
 export default PokemonList;
